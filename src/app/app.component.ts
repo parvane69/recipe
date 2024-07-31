9e4a8b85d9ac7834c3e39b66f52d5abcaec0dc85
parvane <parvaneh@desap.co.uk>
2024-07-31 20:25:40 +0330

    show favorite recipe count in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -9,45 +6,16 @@ import { Recipe } from './models/recipe.model';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  recipes: Recipe[] = [];
-  filteredRecipes: Recipe[] = [];
-  isLoading = true;
-  error: string | null = null;
-  searchForm: FormGroup;
-
-  
-  constructor(private http: HttpClient, private fb: FormBuilder) {
-    this.searchForm = this.fb.group({
-      name: [''],
-      ingredient: [''],
-    });
-  }
+  savedRecipesCount: number = 0;
 
   ngOnInit() {
-    this.http.get<Recipe[]>('/assets/recipes.json').subscribe(
-      (data) => {
-        this.recipes = data;
-        this.filteredRecipes = data;
-        this.isLoading = false;
-      },
-      (error) => {
-        this.error = 'Failed to load recipes';
-        this.isLoading = false;
-      }
-    );
-
-    this.searchForm.valueChanges.subscribe((value) => {
-      this.filterRecipes(value);
-    });
+    this.updateSavedRecipesCount();
   }
 
-  filterRecipes(value: { name: string; ingredient: string }) {
-    this.filteredRecipes = this.recipes.filter(
-      (recipe) =>
-        recipe.name.toLowerCase().includes(value.name.toLowerCase()) &&
-        recipe.ingredients.some((ingredient) =>
-          ingredient.toLowerCase().includes(value.ingredient.toLowerCase())
-        )
-    );
+  updateSavedRecipesCount() {
+    const recipes = localStorage.getItem('savedRecipes');
+    if (recipes) {
+      this.savedRecipesCount = JSON.parse(recipes).length;
+    }
   }
 }
