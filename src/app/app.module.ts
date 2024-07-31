b77210c705ac76900d15389ea915eab1d804bc60
parvane <parvaneh@desap.co.uk>
2024-07-31 09:32:14 +0330

    highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeCardComponent
+    RecipeCardComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
